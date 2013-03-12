03f6f2ca0f6c0c1303d569f591dba4f4a99499e7
Lomar Berry <lxberry@gmail.com>
2013-03-12 06:38:59 -0400

    fix possible cause of media_profile messups... -_-

--- device_doubleshot.mk
+++ device_doubleshot.mk
@@ -122,11 +122,6 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
-# Custom media config for HTC camera
-PRODUCT_COPY_FILES += \
-    device/htc/doubleshot/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    device/htc/doubleshot/configs/media_profiles.xml:system/etc/media_profiles.xml
     
 ## misc
 PRODUCT_PROPERTY_OVERRIDES += \
